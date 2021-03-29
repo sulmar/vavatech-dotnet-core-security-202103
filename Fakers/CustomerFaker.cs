c25930bc847e0714e6397671ce1a69a600e9bf4f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-29 09:44:16 +0200

    Fakers

---
+++ Fakers/CustomerFaker.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using Models;
+using System;
+
+namespace Fakers
+{
+    // dotnet add package Bogus
+    public class CustomerFaker : Faker<Customer>
+    {
+        public CustomerFaker()
+        {
+            RuleFor(p => p.Id, f => f.IndexFaker);
+            RuleFor(p => p.FirstName, f => f.Person.FirstName);
+            RuleFor(p => p.LastName, f => f.Person.LastName);
+            RuleFor(p => p.Email, f => f.Person.Email);
+            RuleFor(p => p.PhoneNumber, f => f.Person.Phone);
+            RuleFor(p => p.DateOfBirth, f => f.Person.DateOfBirth);
+            RuleFor(p => p.Username, f => f.Person.UserName);
+            RuleFor(p => p.HashedPassword, f => "12345");
+            RuleFor(p => p.IsRemoved, f => f.Random.Bool(0.2f));
+        }
+    }
+
+    
+}
