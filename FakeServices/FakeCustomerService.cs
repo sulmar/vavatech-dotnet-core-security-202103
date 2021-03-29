774b05923274af873be4fadd0a4d8ae8f53aba54
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-29 09:47:15 +0200

    Start

---
+++ FakeServices/FakeCustomerService.cs
@@ -0,0 +1,38 @@
+﻿using IServices;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FakeServices
+{
+    public class FakeCustomerService : ICustomerService
+    {
+        private ICollection<Customer> customers;
+
+        public IEnumerable<Customer> Get()
+        {
+            return customers;
+        }
+
+        public IEnumerable<Customer> GetActive()
+        {
+            return customers.Where(c => !c.IsRemoved).ToList();
+        }
+    }
+
+    public class FakeOrderService : IOrderService
+    {
+        private ICollection<Order> orders;
+
+        public IEnumerable<Order> Get()
+        {
+            return orders;
+        }
+
+        public IEnumerable<Order> Get(string username)
+        {
+            return orders.Where(o => o.Customer.Username == username).ToList();
+        }
+    }
+}
