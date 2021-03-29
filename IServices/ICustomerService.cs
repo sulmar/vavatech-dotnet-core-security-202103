7b852eed30175c040d6626d0931489ee86f4ec67
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-29 09:47:09 +0200

    Interfejsy

---
+++ IServices/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+
+namespace IServices
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> Get();
+        IEnumerable<Customer> GetActive();
+    }
+
+}
