c87ee7c75b6ac1c25cbb5aa146aff7a5c926435b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-29 10:25:08 +0200

    WebApi

---
+++ WebApi/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using IServices;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/orders")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService orderService;
+
+        public OrdersController(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+
+        // GET http://localhost:5000/api/orders
+        
+       // [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Order> orders = orderService.Get();
+
+            return Ok(orders);
+        }
+    }
+}
